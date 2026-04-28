e9ea03edc9d36b355d5ed193843c7ab6ad3a8689
dastgheybi-smh <dgsmh867@gmail.com>
2026-04-28 11:47:23 +0330

    the static message input bug fixed

--- filters.py
+++ filters.py
@@ -1,7 +1,7 @@
-is_admin = lambda l: lambda F: F.chat_id in l
-not_admin = lambda l: lambda F: not F.chat_id in l
-text = lambda txt: lambda F: F.text == txt
-state = lambda status: lambda F: F.state == status
-chat_id = lambda cid: lambda F: F.chat_id == cid
+is_admin = lambda l: lambda F, li=l: F.chat_id in li
+not_admin = lambda l: lambda F, li=l: not F.chat_id in li
+text = lambda txt: lambda F, txt2=txt : F.text == txt2
+state = lambda status: lambda M, F, stat=status: F.state == stat
+chat_id = lambda cid: lambda F, id=cid: F.chat_id == id
 command = lambda cmd: text(f"/{cmd}")
 start = command("start")
